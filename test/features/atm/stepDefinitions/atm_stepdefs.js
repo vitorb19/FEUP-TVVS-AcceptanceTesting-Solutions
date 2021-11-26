f0a5e58b4c269a6419aa6374fc3e156a1f448942
vitorb19 <vitorhugobarbosa13@gmail.com>
2021-11-26 13:16:19 +0000

    fix typo

--- test/features/atm/stepDefinitions/atm_stepdefs.js
+++ test/features/atm/stepDefinitions/atm_stepdefs.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const { Before, Given, When, Then } = require("@cucumber/cucumber");
 const Bank = require("../../../../src/atm/bank");
-const ATM = require("../../../../src/atm/ATM");
+const ATM = require("../../../../src/atm/atm");
 const Customer = require("../../../../src/atm/customer");
 
 Before({ tags: "@atm" }, function () {
